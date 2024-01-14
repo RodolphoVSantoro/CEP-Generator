ff97b308dd26d30e804a6956b061dff6a767ddb8
RodolphoVSantoro <rodolphovs@gmail.com>
2024-01-14 16:31:56 -0300

    fix: only call fetch for bin file once

--- scripts/cep.js
+++ scripts/cep.js
@@ -4,32 +4,45 @@ let CEPLabel;
 let CEPText;
 let CEPButton;
 let CEPButtonCopy;
+let CEP_BINARY = null;
 
-function getRandomCep(CepBinary) {
-	const ceps = new Int32Array(CepBinary);
-	const cepsView = new DataView(CepBinary);
-	const random = Math.floor(Math.random() * ceps.length);
-	const cep = cepsView.getInt32(random * 4, true).toString().padStart(8, '0');
-	const formattedCEP = cep.slice(0, 5) + '-' + cep.slice(5, 8);
-	return formattedCEP;
-}
+const usesLittleEndian = true;
 
 async function generateCEP() {
-	if (CEPText) {
-		const response = await fetch(url).catch((_) => {
-			CEPText.nodeValue = 'Failed to fetch bin file';
-			return;
-		});
-		if (response.status !== 200) {
-			CEPText.nodeValue = 'Response status: ' + response.status;
-			return;
-		}
-		const parsedBytes = await response.arrayBuffer().catch((_) => {
-			CEPText.nodeValue = 'Failed to parse response';
-			return;
-		});
-		CEPText.nodeValue = getRandomCep(parsedBytes);
+	if (!CEP_BINARY) {
+		CEPText.nodeValue = 'Failed to load ceps file';
+		return;
 	}
+
+	const cepGroups = new Int32Array(CEP_BINARY);
+	const cepsView = new DataView(CEP_BINARY);
+
+	const random = Math.floor(Math.random() * cepGroups.length);
+
+	const cepGroup = cepsView.getInt32(random * 4, usesLittleEndian);
+	const cep = cepGroup.toString().padStart(8, '0');
+
+	const formattedCEP = cep.slice(0, 5) + '-' + cep.slice(5, 8);
+	CEPText.nodeValue = formattedCEP;
+}
+
+async function loadCEPFile() {
+	const response = await fetch(url).catch((_) => {
+		CEPText.nodeValue = 'Failed to fetch bin file';
+		return;
+	});
+	if (response.status !== 200) {
+		CEPText.nodeValue = 'Response status: ' + response.status;
+		return;
+	}
+	const parsedBytes = await response.arrayBuffer().catch((_) => {
+		CEPText.nodeValue = 'Failed to parse response';
+		return;
+	});
+	if (!parsedBytes) {
+		return;
+	}
+	CEP_BINARY = parsedBytes;
 }
 
 function copyToClipboard(text) {
@@ -39,6 +52,7 @@ function copyToClipboard(text) {
 async function mainCEP() {
 	CEPLabel = document.getElementById('CEPLabel');
 	CEPText = document.createTextNode('');
+	await loadCEPFile();
 	await generateCEP();
 	CEPButton = document.getElementById('CEPButton');
 	CEPLabel.appendChild(CEPText);
